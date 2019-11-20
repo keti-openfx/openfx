c1e26e2a1f4cf8285eb39e6db06c1e86699d48b2
Cyy92 <cyy03576@keti.re.kr>
2019-11-20 20:11:23 +0900

    Fixed Openfx runtime NodeJS

--- executor/nodejs/fx_server.js
+++ executor/nodejs/fx_server.js
@@ -1,4 +1,4 @@
-var PROTO_PATH = __dirname + '/pb/fxwatcher.proto';
+var PROTO_PATH = __dirname + '/proto/fxwatcher.proto';
 
 var grpc = require('grpc');
 var protoLoader = require('@grpc/proto-loader');
