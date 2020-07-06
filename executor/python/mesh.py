ac6c0469e319ab51a192c281b712cd5c98c5954d
imxsuu <imxsuu@gmail.com>
2020-07-06 14:12:04 +0900

    feat: POD간 커뮤니케이션 인터페이스 - python
     Refs: #4

---
+++ executor/python/mesh.py
@@ -0,0 +1,11 @@
+import grpc
+
+import fxwatcher_pb2
+import fxwatcher_pb2_grpc
+
+def mesh_call(functionName: str, input):
+    channel = grpc.insecure_channel("{}.openfx-fn:50052".format(functionName))
+    clientStub = fxwatcher_pb2_grpc.FxWatcherStub(channel)
+    serviceRequest = fxwatcher_pb2.Request(input = input, info = fxwatcher_pb2.Info(FunctionName = functionName, Trigger = fxwatcher_pb2.Trigger(Name = "grpc")))
+    result = clientStub.Call(serviceRequest)
+    return result.Output.encode()
--- executor/python/server.py
+++ executor/python/server.py
@@ -6,6 +6,7 @@ import os
 import time
 import signal
 import grpc
+import logging
 
 import fxwatcher_pb2
 import fxwatcher_pb2_grpc
@@ -32,7 +33,7 @@ class FxWatcherServicer(fxwatcher_pb2_grpc.FxWatcherServicer):
 
     def Call(self, request, context):
         if self.userfunc == None:
-            print('Function not found')
+            logging.info('Function not found')
 
         out = self.userfunc(request)
         return fxwatcher_pb2.Reply(Output=out)
@@ -41,10 +42,11 @@ def createLockFile():
     path = "/tmp/.lock"
     open(path, "w")
     os.chmod(path, 0o660)
-    print('Writing lock-file to: '+ path)
+    logging.info('Writing lock-file to: '+ path)
 
 def serve():
     fxwatcherPort = os.getenv('WATCHER_PORT', '50051')
+    fxmeshPort = os.getenv('MESH_PORT', '50052')
     moduleName = os.getenv('HANDLER_FILE', '/openfx/handler/handler.py')
     funcName = os.getenv('HANDLER_NAME', 'Handler')
 
@@ -54,17 +56,19 @@ def serve():
         watcherServer, server)
 
     server.add_insecure_port('[::]:'+fxwatcherPort)
+    server.add_insecure_port('[::]:'+fxmeshPort)
 
     createLockFile()
 
-    print('[fxwatcher] start service.')
+    logging.info('[fxwatcher] start service.')
+    logging.info('[fxmeshserver] start service.')
     server.start()
     try:
         while True:
             time.sleep(_ONE_DAY_IN_SECONDS)
     except KeyboardInterrupt:
         server.stop(0)
-        print("[fxwatcher] received SIGTERM.")
+        logging.info("[fxwatcher] received SIGTERM.")
 
 if __name__ == '__main__':
     serve()
