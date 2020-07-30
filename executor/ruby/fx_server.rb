1337448c8aa9efe45e30c1f444718fdba68c27f2
imxsuu <imxsuu@gmail.com>
2020-07-30 15:31:50 +0900

    feat: POD간 커뮤니케이션 인터페이스 - Ruby
     Refs: #12

--- executor/ruby/fx_server.rb
+++ executor/ruby/fx_server.rb
@@ -1,9 +1,3 @@
-#!/usr/bin/env ruby
-
-# Sample gRPC server that implements the Greeter::Helloworld service.
-#
-# Usage: $ path/to/greeter_server.rb
-
 this_dir = File.expand_path(File.dirname(__FILE__))
 lib_dir = File.join(this_dir, '.')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
@@ -11,13 +5,13 @@ $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 require 'grpc'
 require 'fxwatcher_services_pb'
 
-# load module file
 require './handler'
+require './fx_mesh'
 
-# GreeterServer is simple server that implements the Helloworld Greeter server.
 class FxWatcherServer < Pb::FxWatcher::Service
   def call(req, _unused_call)
     p "[fxwatcher] start service."
+    p "[fxmesh] start service."
     tmp = FxWatcher.Handler(req.input)
     Pb::Reply.new(Output: "#{tmp}")
   end
@@ -36,6 +30,7 @@ createLockFile
 def main
   s = GRPC::RpcServer.new
   s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
+  s.add_http2_port('0.0.0.0:50052', :this_port_is_insecure)
   s.handle(FxWatcherServer)
   # Runs the server with SIGHUP, SIGINT and SIGQUIT signal handlers to 
   #   gracefully shutdown.
